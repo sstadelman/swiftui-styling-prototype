4a542b9da08c3f9bdba25fa75cc58cf3e2f87a1f
Stan Stadelman <sstadelman@gmail.com>
2020-09-11 16:35:46 -0700

    Initial Commit

---
+++ SwiftUIStylingProto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStylingProto
+//
+//  Created by Stan Stadelman on 9/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIStylingProto/SwiftUIStylingProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStylingProtoApp.swift
+//  SwiftUIStylingProto
+//
+//  Created by Stan Stadelman on 9/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStylingProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
